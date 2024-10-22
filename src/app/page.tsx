64769c4d9d5b43fba5304a1c136951bb567a1df0
patisubhakanta <patisubhakantaniku@gmail.com>
2024-10-22 19:41:18 +0530

    Product detials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,9 @@ export default async function Home() {
   return (
     <>
       <div className="container mx-auto p-8">
-        <h1 className="text-3xl font-bold mb-6 text-center">Saleor Products</h1>
+        <div className="w-full rounded flex items-center justify-center bg-blue-700 mb-6 py-2">
+        <h1 className="text-3xl font-bold text-center text-gray-100 italic">Saleor Products</h1>
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {products.map((product: ProductDTO) => (product) && (
             <Link href={`/product/${product.id}`} key={product.id}>
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -26,6 +26,9 @@ const ProductDetails = async ({ params }: { params: { id: string } }) => {
     const { amount, currency } = product.pricing.priceRange.start.gross
     return (
         <div className="container mx-auto p-8">
+            <div className="w-full rounded flex items-center justify-center bg-blue-700 mb-6 py-2">
+                <h1 className="text-3xl font-bold text-center text-gray-100 italic">Product Details</h1>
+            </div>
             <div className="flex flex-col md:flex-row gap-8">
                 <div className="flex-1 border rounded">
                     <Image
